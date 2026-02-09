3dc0740d4e461e5b8ec5ac27f9b65a38f10d45a2
Noah P <noahpersaud@zohomail.com>
2026-02-08 22:01:44 -0500

    Add random names

--- names.py
+++ names.py
@@ -1,9 +1,23 @@
 # would eventually be read from file. split by gender
+import csv
+import random
 
-NAMES = ["Sam", "Alex", "Mia", "Leo", "Ava", "Noah", "Olivia"]
+def load_names():
+    names = {"boys": [], "girls": []}
+    with open(r'process-names\top_names_unique_2021_2024.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            name = row['name'].strip()
+            gender = row['gender'].strip().lower()
+            if gender == "girl":
+                names["girls"].append(name)
+            elif gender == "boy":
+                names["boys"].append(name)
+    return names
+NAMES = load_names()
 
-def get_name(gender="male"):
-    # hard code for now
-    name = NAMES[-1]
-
-    return name
+def get_name(gender):
+    if gender == "girl":
+        return random.choice(NAMES["girls"])
+    else:
+        return random.choice(NAMES["boys"])

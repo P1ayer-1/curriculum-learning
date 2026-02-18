3ec012807e8734ab09d7c825709ed98a9f88afb2
Noah P <noahpersaud@zohomail.com>
2026-02-17 20:29:44 -0500

    Add experiences with placeholder data

---
+++ experiences.py
@@ -0,0 +1,65 @@
+
+
+EXPERIENCES = {
+    "playing_sports": {
+        "min_tier": 1,
+        "max_tier": 6,
+        "description": "playing sports", 
+        "experience_goal": "playing sports",
+        "banned_tones": ["imaginative"],
+        "locations": [
+            "at home", "at school", "during a car ride", "at the library",
+            "in a playroom", "at mealtime", "at the playground", "at daycare",
+            "at the store", "at grandparent's house"
+        ]
+    },
+    "playing_music": {
+        "min_tier": 1,
+        "max_tier": 6,
+        "description": "playing music",
+        "experience_goal": "playing music",
+        "banned_tones": ["imaginative"],
+        "locations": [
+            "at home", "at school", "during a car ride", "at the library",
+            "in a playroom", "at mealtime", "at the playground", "at daycare",
+            "at the store", "at grandparent's house"
+        ]
+    },
+    "watching_sports": {
+        "min_tier": 0,
+        "max_tier": 6,
+        "description": "watching sports",
+        "experience_goal": "watching sports",
+        "banned_tones": ["imaginative"],
+        "locations": [
+            "at home", "at school", "during a car ride", "at the library",
+            "in a playroom", "at mealtime", "at the playground", "at daycare",
+            "at the store", "at grandparent's house"
+        ]
+    },
+    "drawing_or_crafting": {
+        "min_tier": 1,
+        "max_tier": 6,
+        "description": "drawing or crafting",
+        "experience_goal": "drawing or crafting",
+        "banned_tones": ["imaginative"],
+        "locations": [
+            "at home", "at school", "during a car ride", "at the library",
+            "in a playroom", "at mealtime", "at the playground", "at daycare",
+            "at the store", "at grandparent's house"
+        ],
+    },
+    "cooking_or_baking": {
+        "min_tier": 1,
+        "max_tier": 6,
+        "description": "cooking or baking",
+        "experience_goal": "cooking or baking",
+        "banned_tones": ["imaginative"],
+        "locations": [
+            "at home", "at school", "during a car ride", "at the library",
+            "in a playroom", "at mealtime", "at the playground", "at daycare",
+            "at the store", "at grandparent's house"
+            ],
+    },
+
+}

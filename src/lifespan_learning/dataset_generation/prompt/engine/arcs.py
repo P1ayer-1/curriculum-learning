3cd3e9a5669ad6c8ee8ed909a241b9c23a2bff4e
Noah Persaud <88407894+P1ayer-1@users.noreply.github.com>
2026-02-28 21:49:35 -0500

    Refactor Arc classes to include random number generator in initialization

--- src/lifespan_learning/dataset_generation/prompt/engine/arcs.py
+++ src/lifespan_learning/dataset_generation/prompt/engine/arcs.py
@@ -1,17 +1,18 @@
 # arcs.py
 from .generation_configs import PromptConfig, ContentTypeConfig
 from .content_type import ContentType
+import random
 
 
 class Arc(ContentType):
-    def __init__(self, config: ContentTypeConfig):
-        super().__init__(config)
+    def __init__(self, config: ContentTypeConfig, rng: random.Random):
+        super().__init__(config, rng=rng)
 
 
 class BasicLearningArc(Arc):
-    def __init__(self, config: ContentTypeConfig):
-        super().__init__(config)
+    def __init__(self, config: ContentTypeConfig, rng: random.Random):
+        super().__init__(config, rng=rng)
 
 class AdvancedLearningArc(Arc):
-    def __init__(self, config: ContentTypeConfig):
-        super().__init__(config)
+    def __init__(self, config: ContentTypeConfig, rng: random.Random):
+        super().__init__(config, rng=rng)
